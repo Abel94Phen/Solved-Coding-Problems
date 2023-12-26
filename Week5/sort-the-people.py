2d4dad0e58b8f9c1cd62ff97e66d2ff8c8b6f9b5
Abel Begashaw <115411506+Abel94Phen@users.noreply.github.com>
2023-12-26 19:02:48 +0300

    Added solution for Sort the People

--- Week5/sort-the-people.py
+++ Week5/sort-the-people.py
@@ -1,8 +1,10 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        for i in range(len(names) - 1):
-            for j in range(len(names) - 1 - i):
-                if heights[j] < heights[j + 1]:
-                    heights[j], heights[j+1] = heights[j+1],heights[j]
-                    names[j], names[j+1] = names[j+1],names[j]
+        for i in range(len(names)):
+            min_index = i
+            for j in range(i + 1, len(names)):
+                if heights[j] > heights[min_index]:
+                    min_index = j
+            heights[i], heights[min_index] = heights[min_index],heights[i]
+            names[i], names[min_index] = names[min_index],names[i]
         return names
