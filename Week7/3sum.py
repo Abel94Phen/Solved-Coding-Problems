35d0b5304d1dffbaa00e586d86f4dc85229087d0
Abel Begashaw <115411506+Abel94Phen@users.noreply.github.com>
2024-01-08 08:46:00 +0300

    Added solution for 3Sum

---
+++ Week7/3sum.py
@@ -0,0 +1,50 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        result = []
+        for i in range(len(nums)):
+            if i != 0 and nums[i - 1] == nums[i]:
+                continue
+            j, k = i + 1, len(nums) - 1
+            while j < k:
+                if nums[i] + nums[j] + nums[k] == 0:
+                    result.append([nums[i], nums[j], nums[k]])
+                    j += 1
+                    k -= 1
+                    while j < k and nums[j] == nums[j - 1]:
+                        j += 1
+                elif nums[i] + nums[j] + nums[k] < 0:
+                    j += 1
+                else:
+                    k -= 1
+
+        return result            
+#         
+# [-4,-1,-1,0,1,2]
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+#         
