b32cb59799a7f0117002247b2f3b4f2b737a22fb
Abel Begashaw <115411506+Abel94Phen@users.noreply.github.com>
2024-03-01 00:55:37 +0300

    Add solution for Reveal Cards In Increasing Order

---
+++ camp I/leetcode/leetcode/reveal-cards-in-increasing-order.py
@@ -0,0 +1,12 @@
+class Solution:
+    def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
+        deck.sort()
+        result = [0 for _ in range(len(deck))] 
+        queue = deque(range(len(deck)))
+        print(queue)
+        for card in deck:
+            result[queue.popleft()] = card
+            if queue:
+                queue.append(queue.popleft())
+            
+        return result
